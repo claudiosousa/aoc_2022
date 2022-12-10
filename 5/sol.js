b75c52bb6b69b96504f2174493b3c363388ba219
Claudio <claudio.sousa@gmail.com>
2022-12-10 10:06:28 +0100

    day 5, sol 1

--- 5/sol.js
+++ 5/sol.js
@@ -1,14 +1,36 @@
 import { promises as fs } from 'fs';
 
-const full_overlaps = (await fs.readFile('./input.txt', 'utf-8'))
+const [stacks_str, moves_str] = (await fs.readFile('./input.txt', 'utf-8'))
   .toString()
+  .split(/^\n/m);
+
+const stacks = stacks_str
+  .split('\n')
+  .slice(0, -2)
+  .reduce(
+    (acc, line) => {
+      for (let i = 0; i < 9; i++) {
+        const p = 1 + i * 4;
+        if (line.length > p && line[p] != ' ') acc[i].unshift(line[p]);
+      }
+      return acc;
+    },
+    [...new Array(9)].map(() => [])
+  );
+
+const moves = moves_str
   .split('\n')
   .filter(l => l)
-  .map(line => line.split(','))
-  .map(pair => {
-    const [elf1, elf2] = pair.map(r => r.split('-').map(n => parseInt(n)));
-    return covers(elf1, elf2) || covers(elf2, elf1);
-  })
-  .reduce((acc, overlap) => acc + overlap, 0);
+  .map(l => l.match(/([0-9]+)/g).map(n => parseInt(n)));
 
-console.log(full_overlaps);
+for (let [creates, from, to] of moves) {
+  //  console.log(creates, from, to);
+  stacks[to - 1].push(...stacks[from - 1].splice(-creates).reverse());
+  //  console.log(stacks);
+}
+
+const top_creates = stacks
+  .flatMap(c => (c.length == 0 ? [] : c.slice(-1)))
+  .join('');
+
+console.log(top_creates);
