f656a01b6fc1ba4a5e0c359f9bb652ee894815a4
Claudio <claudio.sousa@gmail.com>
2022-12-16 17:56:54 +0100

    day 16, sol 1

---
+++ 16/sol.js
@@ -0,0 +1,44 @@
+import { promises as fs } from 'fs';
+import Queue from '../utils/queue.js';
+
+let valves = (await fs.readFile('./input.txt', 'utf-8'))
+  .toString()
+  .split('\n')
+  .filter(l => l)
+  .map(l => {
+    const matches = l.match(/Valve\s(..).*rate=([0-9]+).*valves?\s(.*)/);
+    return [matches[1], parseInt(matches[2]), matches[3].split(', ')];
+  })
+  .reduce((acc, v) => acc.set(v[0], v.slice(1)), new Map());
+
+let todo = new Queue();
+todo.enqueue(['AA', 0, 30, new Set()]);
+let done = new Set();
+let max_pressure = 0;
+while (!todo.isEmpty) {
+  let [valve, total_pressure, minutes, open] = todo.dequeue();
+  if (minutes <= 1) continue;
+  if (done.has(valve + total_pressure)) continue;
+  done.add(valve + total_pressure);
+  let [valve_pressure, children] = valves.get(valve);
+  for (const child of children) {
+    todo.enqueue([child, total_pressure, minutes - 1, open]);
+  }
+  if (!open.has(valve) && valve_pressure > 0) {
+    total_pressure += valve_pressure * (minutes - 1);
+    max_pressure = Math.max(max_pressure, total_pressure);
+    let open_with_valve = new Set(open);
+    open_with_valve.add(valve);
+    let empty_set = new Set();
+    for (const child of children) {
+      todo.enqueue([
+        child,
+        total_pressure,
+        minutes - 2,
+        open_with_valve,
+        empty_set,
+      ]);
+    }
+  }
+}
+console.log(max_pressure);
---
+++ utils/queue.js
@@ -0,0 +1,39 @@
+class Queue {
+  constructor() {
+    // The actual queue
+    this.elements = {};
+    // The index of the head element
+    this.head = 0;
+    // The index of the tail element
+    this.tail = 0;
+  }
+  enqueue(element) {
+    // Add an element on the current tail index
+    this.elements[this.tail] = element;
+    // Increase the index of the tail element
+    // So the next elements are added at the end
+    this.tail++;
+  }
+  dequeue() {
+    // If the queue is empty, return "undefined"
+    if (this.tail === this.head) {
+      return undefined;
+    }
+    // Pick an element
+    const element = this.elements[this.head];
+    // Delete it
+    delete this.elements[this.head];
+    // Increase the head index
+    this.head++;
+    // Return the element
+    return element;
+  }
+  get size() {
+    return this.tail - this.head;
+  }
+  get isEmpty() {
+    return this.head == this.tail;
+  }
+}
+
+export default Queue;
