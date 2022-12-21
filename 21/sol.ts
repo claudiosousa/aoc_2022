374d6caa0603e3462d4875641b13ed8f3e966a68
Claudio <claudio.sousa@gmail.com>
2022-12-21 10:50:19 +0100

    day 21, sol 2

--- 21/sol.ts
+++ 21/sol.ts
@@ -35,4 +35,31 @@ const getMonkeyValue = (monkey_name: string): number => {
   }
 };
 
-console.log(getMonkeyValue('root'));
+const humain_monkey_name = 'humn';
+const left_monkey_name = (<Expression>monkeys.get('root')).left;
+const right_monkey_name = (<Expression>monkeys.get('root')).right;
+const target_value = getMonkeyValue(right_monkey_name);
+
+const getTreeValueForHValue = (hvalue: number): number => {
+  monkeys.set(humain_monkey_name, hvalue);
+  return getMonkeyValue(left_monkey_name);
+};
+const bsearch = (): number => {
+  let l = 1,
+    r = 2;
+  while (getTreeValueForHValue(r) > target_value) {
+    l = r;
+    r *= 2;
+  }
+
+  do {
+    const c = Math.floor(l + (r - l) / 2);
+    const mval = getTreeValueForHValue(c);
+    if (mval == target_value) return c;
+    if (mval > target_value) l = c;
+    else r = c - 1;
+  } while (l < r);
+  return r;
+};
+
+console.log(bsearch());
